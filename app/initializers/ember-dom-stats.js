f7c325734b8feee5777038d18c5c72b417b729c8
Adam Mokan <amokan@gmail.com>
2015-03-02 21:36:48 -0700

    added delta to stats display

--- app/initializers/ember-dom-stats.js
+++ app/initializers/ember-dom-stats.js
@@ -25,18 +25,40 @@ export default {
     dsText.innerHTML = 'DOM COUNT';
     dsDiv.appendChild( dsText );
 
-    var dsData = document.createElement( 'div' );
-    dsData.id = 'dsData';
-    dsData.style.cssText = 'position:relative;width:74px;height:30px;color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:15px;font-weight:bold;text-align:left;';
-    dsData.innerHTML = '0';
-    dsDiv.appendChild( dsData );
+    var dsCount = document.createElement( 'div' );
+    dsCount.id = 'dsCount';
+    dsCount.style.cssText = 'position:relative;width:74px;height:20px;color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:15px;font-weight:bold;text-align:left;';
+    dsCount.innerHTML = '0';
+    dsDiv.appendChild( dsCount );
+
+    var dsDelta = document.createElement( 'div' );
+    dsDelta.id = 'dsDelta';
+    dsDelta.style.cssText = 'position:relative;width:74px;height:10px;color:#fff;font-family:Helvetica,Arial,sans-serif;font-size:10px;font-weight:bold;text-align:top;';
+    dsDelta.innerHTML = '';
+    dsDiv.appendChild( dsDelta );
 
     document.body.appendChild(container);
 
+    var previousCount = 0;
+    var deltaVal = '';
+
     if (typeof requestAnimationFrame === 'function') {
       requestAnimationFrame(function rAFloop(){
-        var number = parseFloat($("*").length);
-        dsData.textContent = number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1" + ",");
+        var count = parseFloat($("*").length);
+
+        if (count - previousCount != 0) {
+          var delta = count - previousCount;
+          if (delta > 0) {
+            deltaVal = '+' + delta;
+          } else {
+            deltaVal = delta;
+          }
+        }
+
+        dsCount.textContent = count.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1" + ",");
+        dsDelta.textContent = deltaVal;
+        previousCount = count;
+
         requestAnimationFrame(rAFloop);
       });
     }
